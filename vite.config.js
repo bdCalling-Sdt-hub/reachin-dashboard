cfc3a1443c5403171d24e520fe61afdac79ac665
Nadir Hossain <nadirhossain336@gmail.com>
2025-01-20 15:24:57 +0600

    refactor: swap host configuration in vite.config.js for server setup

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "10.0.80.75",
-    // host: "178.16.138.188",
+    // host: "10.0.80.75",
+    host: "178.16.138.188",
     port: 3003,
   },
 });
